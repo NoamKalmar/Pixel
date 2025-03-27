1a2af81a6d7184a706120c9f876d29221fe565bb
NoamKalmar <noamkalmar@gmail.com>
2025-03-27 22:05:50 +0200

    added a servo dataclass and changed the move servo method to be able to run any sequence

--- pixel/servos_manager.py
+++ pixel/servos_manager.py
@@ -1,61 +1,71 @@
 from pyfirmata import Arduino, ArduinoMega, SERVO
 import time
 import threading
+from dataclasses import dataclass
+from collections.abc import Iterable
+
+@dataclass
+class Servo:
+    pin: int
+    current_angle: int = 0
+    is_moving: bool = False
+    should_stop_moving: bool = False
+
 
 class ServosManager:
-    def __init__(self, arduino: Arduino | ArduinoMega, pins: list, start_angle: int = 0, default_angles: dict = None):
-        self.pins = pins
+    def __init__(self, arduino: Arduino | ArduinoMega, pins: tuple, default_angle: int = 0):
+        self.servos = []
         self.arduino = arduino
-        self.start_angle = start_angle
-        self.default_angles = default_angles
-        self.current_angles = [start_angle for _ in range(len(self.pins))]
-        self.is_moving = [False for _ in range(len(self.pins))]
-        self.should_stop_moving = [False for _ in range(len(self.pins))]
-        self.init_servos()
+        self.default_angle = default_angle
+        self.init_servos(pins)
 
-    def init_servos(self) -> None:
-        for i, pin in enumerate(self.pins):
+    def init_servos(self, pins: tuple) -> None:
+        for i, pin in enumerate(pins):
+            servo = Servo(pin, self.default_angle)
+            self.servos.append(servo)
             self.arduino.digital[pin].mode = SERVO
-            self.write_servo(i, self.start_angle)
-    
-    def write_all(self, angle: list) -> None:
-        for i in range(self.pins):
-            if angle[i] is None:
-                continue
-            self.write_servo(i, angle)
-    
+            self.write_servo(i, self.default_angle)
+
     def write_servo(self, index: int, angle: int, part_of_move: bool = False) -> None:
         if not part_of_move:
             self.stop_moving(index)
         angle = max(min(angle, 180), 0) # Keep angle between 0 and 180
-        self.arduino.digital[self.pins[index]].write(angle)
-        self.current_angles[index] = angle
+        self.arduino.digital[self.servos[index].pin].write(angle)
+        self.servos[index].current_angle = angle
+
+    def write_all(self, angle: int) -> None:
+        for i in range(len(self.servos)):
+            self.write_servo(i, angle)
 
     def write_default(self) -> None:
-        self.write_all(self.default_values)
+        self.write_all(self.default_angle)
 
-    def move_servo(self, index: int, target_value: int, rate: float = 0.01) -> None:
-        moving_thread = threading.Thread(target=self._move_servo, args=(index, target_value, rate))
+    def play_sequence(self, index: int, sequence: Iterable, rate: float = 0.01) -> None:
+        moving_thread = threading.Thread(target=self._play_sequence, args=(index, sequence, rate))
         moving_thread.start()
 
-    def _move_servo(self, index: int, target_angle: int, rate: float = 0.01) -> None:
+    def _play_sequence(self, index: int, sequence: Iterable, rate: float = 0.01) -> None:
         self.stop_moving(index)
-        start_angle = self.current_angles[index]
-        step = 1 if target_angle > start_angle else -1
-        self.is_moving[index] = True
-        for angle in range(self.current_angles[index], target_angle + 1, step):
-            if self.should_stop_moving[index]:
+        self.servos[index].is_moving = True
+        for angle in sequence:
+            if self.servos[index].should_stop_moving:
                 break
             self.write_servo(index, angle, True)
             time.sleep(rate)
-        self.is_moving[index] = False
+        self.servos[index].is_moving = False
+    
+    def move_servo(self, index: int, target_angle: int, rate: int = 0.01):
+        current_angle = self.servos[index].current_angle
+        step = 1 if target_angle > current_angle else -1
+        sequence = range(self.servos[index].current_angle, target_angle + 1, step)
+        self.play_sequence(index, sequence, rate)
 
     def stop_moving(self, index: int) -> None:
-        if not self.is_moving[index]:
+        if not self.servos[index].is_moving:
             return
-        self.should_stop_moving[index] = True
-        while self.is_moving[index]: pass # Wait until the moving thread got the message and stopped
-        self.should_stop_moving[index] = False
+        self.servos[index].should_stop_moving = True
+        while self.servos[index].is_moving: pass # Wait until the moving thread got the message and stopped
+        self.servos[index].should_stop_moving = False
 
 class RobotServosManager(ServosManager):
     def __init__(self, 
