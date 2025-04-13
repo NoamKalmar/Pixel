7a639b5daf15e8a7abb9b9f9b4bb333fafec1403
NoamKalmar <noamkalmar@gmail.com>
2025-04-13 16:09:35 +0300

    adding the build file (changed it and also i think i forgot to add it to the last commit)

--- controller_app/build.bat
+++ controller_app/build.bat
@@ -1,1 +1,6 @@
+@REM Building using pyinstaller and keeping only the exe
 pyinstaller app.py --windowed --onefile -i "icon.png" -n "Pixel Controller"
+copy "dist\Pixel Controller.exe" .
+rmdir /s /q build
+rmdir /s /q dist
+del ".\Pixel Controller.spec"
